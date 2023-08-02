757722756810774c1b3698d14210f45a7006a6b0
Anna-Voronina <anna.vs2512@gmail.com>
2023-08-02 21:35:06 +0300

    ADDED: Global styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'stylesheet/theme';
+import { GlobalStyles } from 'stylesheet/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pavuchky-frontend">
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/pavuchky-frontend">
+        <App />
+      </BrowserRouter>
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/stylesheet/GlobalStyles.js
@@ -0,0 +1,84 @@
+import 'modern-normalize';
+import { createGlobalStyle } from 'styled-components';
+import eUkraineLight from '../assets/fonts/e-Ukraine-Light.otf';
+import eUkraineRegular from '../assets/fonts/e-Ukraine-Regular.otf';
+import eUkraineHeadLight from '../assets/fonts/e-UkraineHead-Light.otf';
+import eUkraineHeadRegular from '../assets/fonts/e-UkraineHead-Regular.otf';
+
+export const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'e-Ukraine-Light';
+  src: url(${eUkraineLight});
+}
+
+@font-face {
+  font-family: 'e-Ukraine-Regular';
+  src: url(${eUkraineRegular});
+}
+
+@font-face {
+  font-family: 'e-Ukraine-Head-Light';
+  src: url(${eUkraineHeadLight});
+}
+
+@font-face {
+  font-family: 'e-Ukraine-Head-Regular';
+  src: url(${eUkraineHeadRegular});
+}
+
+body {
+  min-height: 100vh;  
+  margin: 0;
+  font-family: 'e-Ukraine-Light', sans-serif;
+  font-size: 20px;
+  font-style: normal;  
+  font-weight: 300;
+  line-height: normal;
+  color: #2B2B2B;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+ul,
+li {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+  display: block;
+  color: inherit;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+button,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+button {
+  padding: 0;
+  border: none;
+  font-family: inherit;
+  color: inherit;
+  background-color: transparent;
+  cursor: pointer;
+}
+`;
