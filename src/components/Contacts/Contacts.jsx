2151e3ad252b8bde0447da4d7f15b6202b36fcb9
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-03 13:34:22 +0200

    add image

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import location from './ImgLocation/location_s_mob_@.png';
 
 const Contacts = () => {
-  const contactsData = [
-    { name: 'John Doe', phone: '+1234567890', address: '123 Main St, City' },
-    { name: 'Jane Smith', phone: '+9876543210', address: '456 Oak Ave, Town' },
-   
-  ];
-
   return (
     <div>
-      <h1>Contacts</h1>
+      <h3>Де нас знайти</h3>
+      <p>Локація плетіння</p>
       <ul>
-        {contactsData.map((contact, index) => (
-          <li key={index}>
-            <h2>{contact.name}</h2>
-            <p>Phone: {contact.phone}</p>
-            <p>Address: {contact.address}</p>
-          </li>
-        ))}
+        <p>с. Софіївська Борщагівка: ЖК ‘’Софіївський квартал’’</p>
+        <li>вул.Шалімова, 63a (підвал будинку)</li>
+        <li>Дитячий майданчик дит.садочку "Мозаїка"</li>
       </ul>
+      <p>ЖК ‘’Уютний квартал’’ - уточнюється</p>
+      <p> пр-т Героїв Небесної Сотні, 16/22</p>
+      <div>
+        <p>Контактні дані</p>
+        <p>Aдміністратор: Ольга Кузакова</p>
+        <p>+380635693058</p>
+      </div>
+      <div>
+        <img src={location} alt="Location" width="327" height="220" />
+      </div>
+      <div>
+        <p>Слідкуй за нами тут:</p>
+        <svg width="24" height="24">
+          <use href="../../assets/images/sprite.svg#facebook" />
+        </svg>
+        <svg width="24" height="24">
+          <use xlinkHref="#twitter" />
+        </svg>
+      </div>
     </div>
   );
 };
