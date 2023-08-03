6d7a1c1c55cdb4bce17ed24edf2d33fbda7d9d87
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-03 19:05:36 +0200

    updating styles

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,9 +5,14 @@ import {
   ContactsContainer,
   ContactsTitle,
   ContactsSubTitle,
-    ContactsFirstAdress,
-    ContactsFirstAdressList,
+  ContactsFirstAdress,
+  ContactsFirstAdressList,
   ContactsSecondAdress,
+  ContactPerson,
+  ImgLocationContainer,
+  SocialMediaContainer,
+  SocialMediaTitle,
+  SocialMediaLinks,
 } from './Contacts.styled';
 
 const Icon = ({ name, color, size }) => (
@@ -23,33 +28,41 @@ const Contacts = () => {
       <ContactsSubTitle>Локація плетіння</ContactsSubTitle>
       <ul>
         <ContactsFirstAdress>
-          c. Софіївська Борщагівка: ЖК ‘’Софіївський квартал’’
+          c. Софіївська Борщагівка: ЖК &#8220;Софіївський квартал&#8221;
         </ContactsFirstAdress>
-        <ContactsFirstAdressList>вул.Шалімова, 63a (підвал будинку)</ContactsFirstAdressList>
-        <ContactsFirstAdressList>Дитячий майданчик дит.садочку "Мозаїка"</ContactsFirstAdressList>
+        <ContactsFirstAdressList>
+          вул.Шалімова, 63a (підвал будинку)
+        </ContactsFirstAdressList>
+        <ContactsFirstAdressList>
+          Дитячий майданчик дит.садочку &#8220;Мозаїка&#8221;
+        </ContactsFirstAdressList>
       </ul>
-      <ContactsSecondAdress>ЖК ‘’Уютний квартал’’ - уточнюється</ContactsSecondAdress>
-      <ContactsSecondAdress> пр-т Героїв Небесної Сотні, 16/22</ContactsSecondAdress>
+      <ContactsSecondAdress>
+        ЖК &#8220;Уютний квартал&#8221; - уточнюється пр-т Героїв Небесної
+        Сотні, 16/22
+      </ContactsSecondAdress>
       <div>
         <ContactsSubTitle>Контактні дані</ContactsSubTitle>
-        <p>Aдмiнicтpaтop: Ольга Кузакова</p>
-        <p>+380635693058</p>
+        <ContactPerson>Aдмiнicтpaтop: Ольга Кузакова</ContactPerson>
+        <ContactPerson>+380635693058</ContactPerson>
       </div>
-      <div>
+      <ImgLocationContainer>
         <img src={location} alt="Location" width="327" height="220" />
-      </div>
-      <div>
-        <p>Слідкуй за нами тут:</p>
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-          <Icon name="facebook" size={24} />
-        </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-          <Icon name="telegram" size={24} />
-        </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-          <Icon name="instagram" size={24} />
-        </a>
-      </div>
+      </ImgLocationContainer>
+      <SocialMediaContainer>
+        <SocialMediaTitle>Слідкуй за нами тут:</SocialMediaTitle>
+        <SocialMediaLinks>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <Icon name="facebook" size={24} />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <Icon name="telegram" size={24} />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <Icon name="instagram" size={24} />
+          </a>
+        </SocialMediaLinks>
+      </SocialMediaContainer>
     </ContactsContainer>
   );
 };
--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 import { theme } from '../../stylesheet/theme';
-const { fontSizes, fontWeights, colors, lineHeights } = theme;
+const { fontSizes, fontWeights, colors } = theme;
 
 // export const OurNeedsItemLi = styled.li`
 //   width: 100%;
@@ -14,12 +14,23 @@ const { fontSizes, fontWeights, colors, lineHeights } = theme;
 //   padding: 16px 12px;`
 
 export const ContactsContainer = styled.div`
-  width: 375px;
-  padding: 28px 16px 31px 16px;
+  padding: 28px 16px;
   margin-top: 36px;
   margin-bottom: 36px;
   border-radius: 16px;
   background: ${colors.gradientBackground};
+  @media only screen and (min-width: 767px) {
+    margin-top: 44px;
+    margin-bottom: 44px;
+    max-width: 768px;
+    padding: 0 24px;
+  }
+  @media only screen and (min-width: 1200px) {
+    margin-top: 64px;
+    margin-bottom: 64px;
+    max-width: 1200px;
+    padding: 0 16px;
+  }
 `;
 
 export const ContactsTitle = styled.h3`
@@ -51,10 +62,30 @@ export const ContactsFirstAdressList = styled.li`
 export const ContactsSecondAdress = styled.p`
   font-size: ${fontSizes.xs};
   font-weight: ${fontWeights.light};
+  margin-bottom: 16px;
 `;
-export const ContactsUs = styled.div`
- 
-
+export const ContactPerson = styled.p`
+ font-size: ${fontSizes.xs};
+font-weight: ${fontWeights.light};
+ margin-bottom: 8px;
+`;
+export const ImgLocationContainer = styled.div`
   margin-top: 16px;
-  margin-bottom: 8px;
+  margin-bottom: 16px;
+`;
+export const SocialMediaContainer = styled.div`
+  display: flex;
+`;
+export const SocialMediaTitle = styled.p`
+  font-size: ${fontSizes.xs};
+  font-weight: ${fontWeights.light};
+
+`;
+
+export const SocialMediaLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin-left: 16px;
+  align-items: center;
 `;
