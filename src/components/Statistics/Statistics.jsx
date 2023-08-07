9bc21fb4d8d3f2fe11f0d24a71aa4eeb3a649cda
Anna-Voronina <anna.vs2512@gmail.com>
2023-08-07 12:54:44 +0300

    UPDATED: Statistics styles for mobile and tablet

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,45 @@
 import { useMediaQuery } from 'react-responsive';
-import { StatisticsSection } from './Statistics.styled';
-import { StatisticsMobile } from 'components/StatisticsMobile/StatisticsMobile';
+import {
+  AchievementsList,
+  AchievementsListItem,
+  AchievementsNumber,
+  AchievementsTitle,
+  StatisticsSection,
+} from './Statistics.styled';
+import { StatisticsDesktop } from 'components/StatisticsMobile/StatisticsDesktop';
 
 export const Statistics = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
 
   return (
-    <StatisticsSection>{isMobile && <StatisticsMobile />}</StatisticsSection>
+    <StatisticsSection>
+      {isDesktop ? (
+        <StatisticsDesktop />
+      ) : (
+        <>
+          <AchievementsTitle>Наші досягнення</AchievementsTitle>
+          <AchievementsList>
+            <AchievementsListItem>
+              Висока якість матеріалів
+            </AchievementsListItem>
+            <AchievementsListItem>
+              <AchievementsNumber>150&#43;</AchievementsNumber> днів у справі
+            </AchievementsListItem>
+            <AchievementsListItem>
+              Підбір матеріалів та кольорової гами
+            </AchievementsListItem>
+            <AchievementsListItem>
+              <AchievementsNumber>3500 кв.м &#43;</AchievementsNumber> сітки
+              сплетено
+            </AchievementsListItem>
+            <AchievementsListItem>Вдоконалення технологій</AchievementsListItem>
+            <AchievementsListItem>
+              <AchievementsNumber>50&#43;</AchievementsNumber> учасників
+              ініціативи
+            </AchievementsListItem>
+          </AchievementsList>
+        </>
+      )}
+    </StatisticsSection>
   );
 };
---
+++ src/components/StatisticsMobile/StatisticsDesktop.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const AdvantagesList = styled.ul``;
+
+export const StatisticsList = styled.ul``;
