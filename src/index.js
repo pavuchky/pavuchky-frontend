08a0543d34a79d5e344c55244f013c8b30fdc94d
Anna-Voronina <anna.vs2512@gmail.com>
2023-08-09 17:24:52 +0300

    ADDED: React toastify

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
 import { LanguageProvider } from 'utils/LanguageContext';
-import { App } from 'App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'stylesheet/theme';
 import { GlobalStyles } from 'stylesheet/GlobalStyles';
+import { ToastContainer } from 'react-toastify';
+import { App } from 'App';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <BrowserRouter basename="/pavuchky-frontend">
           <LanguageProvider>
             <App />
+            <ToastContainer autoClose={2000} />
           </LanguageProvider>
         </BrowserRouter>
         <GlobalStyles />
