2d35e4bb152dc871095579eb974d8635f3df8f3b
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-10 09:46:44 +0200

    start

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,11 +1,21 @@
 import { Suspense } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
 const GalleryPage = () => {
   return (
+    <>
+      <h3>Галерея</h3>
+      <div>
+        <NavLink to={"/photos"}>Фото</NavLink>
+       <NavLink to={"/videos"}>Відео</NavLink>
+       
+      </div>
+     
     <Suspense fallback={null}>
       <Outlet />
-    </Suspense>
+      </Suspense>
+    </>
   );
 };
 
