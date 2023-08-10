419430bfa4747c9bbf2a113a96b7b80cf341570e
Anna-Voronina <127735848+Anna-Voronina@users.noreply.github.com>
2023-08-10 13:38:47 +0000

    Deploying to gh-pages from @ pavuchky/pavuchky-frontend@e8d13cf87c803635bc7477f8f0780731a8f81751 🚀

--- static/js/177.1ee0f5aa.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpavuchky_frontend=self.webpackChunkpavuchky_frontend||[]).push([[177],{9177:function(n,e,r){r.r(e);var u=r(184);e.default=function(){return(0,u.jsx)("div",{children:"PartnersPage"})}}}]);
-//# sourceMappingURL=177.1ee0f5aa.chunk.js.map
