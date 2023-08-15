1b2826d2853bbc085abe938a94bf86ff4c3b3041
Anna-Voronina <127735848+Anna-Voronina@users.noreply.github.com>
2023-08-15 13:52:54 +0000

    Deploying to gh-pages from @ pavuchky/pavuchky-frontend@4b3260edbdc1efc838603c4c3d2e26b2e9e64315 🚀

--- static/js/640.0d024249.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpavuchky_frontend=self.webpackChunkpavuchky_frontend||[]).push([[640],{1640:function(n,e,u){u.r(e);var r=u(184);e.default=function(){return(0,r.jsx)("div",{children:"GalleryPhotos"})}}}]);
-//# sourceMappingURL=640.0d024249.chunk.js.map
