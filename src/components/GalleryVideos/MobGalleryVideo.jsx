40127eaefcdffdc77a75969a8030f0b3d89e7c0c
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-21 11:10:51 +0200

    Fix scroll

--- src/components/GalleryVideos/MobGalleryVideo.jsx
+++ src/components/GalleryVideos/MobGalleryVideo.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Scrollbar } from 'swiper';
+import React, { useState } from 'react';
+import { Scrollbar} from 'swiper';
 import 'swiper/css/bundle';
 
 import {
@@ -11,31 +11,40 @@ import {
 } from './MobGalleryVideo.styled';
 
 const MobGalleryVideos = ({ data }) => {
+  const [playingVideoId, setPlayingVideoId] = useState(null);
+
+  const handleVideoClick = (id) => {
+    setPlayingVideoId(id);
+  };
+    
   return (
     <VideoGallerySwiperWrapper>
       <VideoSwiperContainer
+        
         spaceBetween={16}
         slidesPerView={1}
         scrollbar={{ draggable: true }}
         modules={[Scrollbar]}
-       
+       clickable= {true}
       >
         {data?.galleryVideoList?.map(({ id, videoLink }) => (
-          <VideoSwiperSlideContainer key={id}>
+          <VideoSwiperSlideContainer key={id}  >
+            <div  onClick={() => handleVideoClick(id)}>
             <VideoReactPlayer
-             
+            
+         style={{ pointerEvents: 'none' }}
               url={videoLink}
               width={320}
-              playing={false}
+              playing={id === playingVideoId}
               muted={true}
-              controls={true}
-              loop={true}
+              controls={false}
+              loop={false}
               config={{
                 youtube: {
                   playerVars: { origin: 'https://www.youtube.com' },
                 },
               }}
-            />
+            /></div>
             <VideoText>
               Виготовлення <br /> маскувальних сіток
             </VideoText>
